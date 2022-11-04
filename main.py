47240d5b1779c2aec459f5e49ff142f10e74ba4e
Eagle_Vector <kumarsubhambili3@gmail.com>
2022-11-04 17:41:22 +0530

    checkpoint

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.exception import SensorException
+from sensor.logger import logging
+import os, sys
 
 if __name__ == '__main__':
-    
-    training_pipeline = TrainPipeline()
-    training_pipeline.run_pipeline()
+    try:
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
+
+    except Exception as e:
+        logging.exception(e)
