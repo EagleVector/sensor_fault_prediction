0778b6842f32c5e0acebd71c89e91d59758164af
Eagle_Vector <kumarsubhambili3@gmail.com>
2022-11-04 17:41:37 +0530

    data_validation

--- sensor/components/data_validation.py
+++ sensor/components/data_validation.py
@@ -3,8 +3,9 @@ from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationA
 from sensor.entity.config_entity import DataValidationConfig
 from sensor.exception import SensorException
 from sensor.logger import logging
-from sensor.utils.main_utils import read_yaml_file
+from sensor.utils.main_utils import read_yaml_file, write_yaml_file
 import os, sys
+from scipy.stats import ks_2samp
 import pandas as pd
 
 class DataValidation:
@@ -23,8 +24,9 @@ class DataValidation:
 
     def validate_number_of_columns(self, dataframe:pd.DataFrame)->bool:
         try:
-            number_of_columns = self._schema_config['columns']
-
+            number_of_columns = len(self._schema_config['columns'])
+            logging.info(f"Required number of columns: {number_of_columns}")
+            logging.info(f"DataFrame has {len(dataframe.columns)}")
             if len(dataframe.columns) == number_of_columns:
                 return True
             return False
@@ -58,8 +60,34 @@ class DataValidation:
             raise SensorException(e, sys)
 
 
-    def detect_dataset_drift(self):
-        pass
+    def detect_dataset_drift(self, base_df, current_df, threshold=0.05):
+        try:
+            status = True
+            report = {}
+            for column in base_df.columns:
+                d1 = base_df[column]
+                d2 = current_df[column]
+                is_same_dist = ks_2samp(d1, d2)
+                if threshold <= is_same_dist.pvalue:
+                    is_found = False
+                else:
+                    is_found = True
+                    status = False
+                report.update({column:{
+                    "p_value":float(is_same_dist.pvalue),
+                    "drift_status":is_found
+                    
+                    }})
+
+            drift_report_file_path = self.data_validation_config.drift_report_file_path
+
+            #Create directory
+            dir_path = os.path.dirname(drift_report_file_path)
+            os.makedirs(dir_path, exist_ok = True)
+            write_yaml_file(file_path=drift_report_file_path, content=report)
+            return status
+        except Exception as e:
+            raise SensorException(e, sys)
 
     def initiate_data_validation(self)->DataValidationArtifact:
         try:
@@ -95,7 +123,21 @@ class DataValidation:
             if len(error_message)>0:
                 raise Exception(error_message)
 
-            #Checking the data drift.
+            #Checking the data drift
+            self.detect_dataset_drift(base_df=train_dataframe, current_df=test_dataframe)
 
+            data_validation_artifact = DataValidationArtifact(
+                validation_status=status,
+                valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
+                valid_test_file_path=self.data_ingestion_artifact.test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path=None,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path,
+            )
+
+            logging.info(f"Data validation artifact: {data_validation_artifact}")
+
+            return data_validation_artifact
+        
         except Exception as e:
             raise SensorException(e,sys)
