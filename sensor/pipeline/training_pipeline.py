cf74f6bad96512fc985b9a2dd51905808e893047
Eagle_Vector <kumarsubhambili3@gmail.com>
2022-11-06 13:56:35 +0530

    train_pipeline

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,12 +1,12 @@
-from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig
-from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelTrainerConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact
 from sensor.exception import SensorException
 import os, sys
 from sensor.logger import logging
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
 from sensor.components.data_transformation import DataTransformation
-
+from sensor.components.model_trainer import ModelTrainer
 
 class TrainPipeline:
 
@@ -46,9 +46,12 @@ class TrainPipeline:
         except Exception as e:
             raise SensorException(e, sys)
 
-    def start_model_trainer(self):
+    def start_model_trainer(self, data_transformation_artifact:DataTransformationArtifact):
         try:
-            pass
+            model_trainer_config = ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config)
+            model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
         except Exception as e:
             raise SensorException(e, sys)
 
@@ -69,5 +72,6 @@ class TrainPipeline:
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
         except Exception as e:
             raise SensorException(e, sys)
--- sensor/utils/main_utils.py
+++ sensor/utils/main_utils.py
@@ -51,7 +51,6 @@ def load_numpy_array_data(file_path: str) -> np.array:
     except Exception as e:
         raise SensorException(e, sys) from e
 
-
 def save_object(file_path: str, obj: object) -> None:
     try:
         logging.info("Entered the save_object method of MainUtils class")
@@ -59,5 +58,14 @@ def save_object(file_path: str, obj: object) -> None:
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
         logging.info("Exited the save_object method of MainUtils class")
+    except Exception as e:
+        raise SensorException(e, sys) from e
+
+def load_object(file_path: str, ) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} is not exists")
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise SensorException(e, sys) from e
